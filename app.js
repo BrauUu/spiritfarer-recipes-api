67766ef9c538a4dd4bec0e4db24689c8a010abcc
Brayan Frank <brayanfrank.sm@gmail.com>
2022-02-20 22:41:14 -0300

    port change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
-const port = 3001
+const port = 4001
 
 server.listen(port, () => {
   console.log(`running on port ${port}`)
